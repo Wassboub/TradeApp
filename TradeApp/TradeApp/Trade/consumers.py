13477c50dce1825223e81efbc2f15662654e7f5d
Wass <81308535+Wassboub@users.noreply.github.com>
2023-04-01 02:40:34 +0100

    Update consumers.py

--- TradeApp/TradeApp/Trade/consumers.py
+++ TradeApp/TradeApp/Trade/consumers.py
@@ -38,9 +38,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
 		username = text_data_json["username"]
 		print('chat 1')
 		if(username == "binance"):
-			await self.create_coinsInfo(message)
 			#twohours = datetime.now() + timedelta(hours=2)
 			message["t"] = datetime.now().strftime('%d/%m/%Y %H:%M')
+			await self.create_coinsInfo(message)
 			await self.channel_layer.group_send(
 				self.roomGroupName,{
 					"type" : "sendMessage" ,
