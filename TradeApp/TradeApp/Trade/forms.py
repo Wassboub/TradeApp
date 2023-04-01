b7f40983b274d01586434c1194e5186476a89cca
Wass <81308535+Wassboub@users.noreply.github.com>
2023-04-01 15:28:33 +0100

    Update forms.py

--- TradeApp/TradeApp/Trade/forms.py
+++ TradeApp/TradeApp/Trade/forms.py
@@ -7,5 +7,5 @@ class LoginForm(forms.Form):
     username = forms.CharField(label='',widget=forms.TextInput(attrs={'class':'form-control un','placeholder': 'Username', 'style': 'width: 300px;'}))
     password = forms.EmailField(label='',widget=forms.EmailInput(attrs={'class':'form-control pass','placeholder' :'Password','type':'password' ,'style': 'width: 300px;'}))
     
-#class CoinForm(forms.Form):
-     #Coin= forms.CharField(label='', widget=forms.Select(attrs={'class':"btn btn-secondary dropdown-toggle"},choices=Coins.objects.values_list('symbol','symbol').order_by('symbol')))
+class CoinForm(forms.Form):
+     Coin= forms.CharField(label='', widget=forms.Select(attrs={'class':"btn btn-secondary dropdown-toggle"},choices=Coins.objects.values_list('symbol','symbol').order_by('symbol')))
