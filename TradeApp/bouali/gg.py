e092965947cb4482a7ee3ed34b26f8fd5a1fcacd
Wass <81308535+Wassboub@users.noreply.github.com>
2023-05-09 22:28:17 +0100

    Update gg.py

--- TradeApp/bouali/gg.py
+++ TradeApp/bouali/gg.py
@@ -1,10 +1,13 @@
 from binance.client import Client
 from datetime import datetime
 import time
-import telegram
 import requests
 
-
+last_reset_time15m = datetime.now()
+last_reset_time1h = datetime.now()
+last_reset_time4h = datetime.now()
+last_reset_time1d = datetime.now()
+last_reset_time3d = datetime.now()
 # Set up API key and secret
 api_key = 'your_api_key'
 api_secret = 'your_api_secret'
@@ -13,21 +16,14 @@ api_secret = 'your_api_secret'
 client = Client(api_key, api_secret)
 
 # Initialize variables
-buy_volume_15m = 0
-sell_volume_15m = 0
-buy_volume_1h = 0
-sell_volume_1h = 0
-buy_volume_4h = 0
-sell_volume_4h = 0
+
 processed_trade_ids = set()
 processed_trade_ids2 = set()
-period_length_15m = 900  # 15 minutes
-period_length_1h = 3600  # 1 hour
-period_length_4h = 14400  # 4 hours
-periods_15m = [0] * 10  # Initialize periods list to keep track of net volumes for the last 10 periods
-periods_1h = [0] * 10
-periods_4h = [0] * 10
-current_period_index = 0
+net_volume_4h=0
+net_volume_1h=0
+net_volume_15m=0
+net_volume_1d=0
+net_volume_3d=0
 # Reset the variables
 buy_volume = 0
 sell_volume = 0
@@ -35,11 +31,29 @@ bot_token = '6276656217:AAG2ULfvYbe-vito2JWHKYHBLpo__EuoZ1Q'
 chat_id = '-1001965574985'
 # Run the loop continuously
 while True:
+
     try:
+
         # Retrieve the latest trades for the BTC/USDT pair
         trades = client.get_recent_trades(symbol='BTCUSDT', limit=800)
         # Calculate the total volume of executed large trades for buy and sell
         for trade in trades:
+            x=trade['id']
+            if float(trade['qty']) > 0.01 and trade['id'] not in processed_trade_ids2:  # Trade volume threshold is set to 1 BTC and check if the trade has been processed before
+                processed_trade_ids2.add(trade['id'])
+
+                if trade['isBuyerMaker']:  # Check if it's a buy or sell order
+                    net_volume_4h += float(trade['qty'])
+                    net_volume_1h += float(trade['qty'])
+                    net_volume_15m += float(trade['qty'])
+                    net_volume_1d += float(trade['qty'])
+                    net_volume_3d += float(trade['qty'])
+                else:
+                    net_volume_4h -= float(trade['qty'])
+                    net_volume_1h -= float(trade['qty'])
+                    net_volume_15m -= float(trade['qty'])
+                    net_volume_1d -= float(trade['qty'])
+                    net_volume_3d -= float(trade['qty'])
             if float(trade['qty']) > 0.1 and trade[ 'id'] not in processed_trade_ids:  # Trade volume threshold is set to 1 BTC and check if the trade has been processed before
              processed_trade_ids.add(trade['id'])  # Add the trade ID to the set of processed trades to avoid processing it again in the future
 
@@ -51,87 +65,81 @@ while True:
 
              if float(trade['qty']) > 10:
                  if trade['isBuyerMaker']:  # Check if it's a buy or sell order
-                     message = f"🟢🟢 *whales just detected `{float(trade['qty']):.2f}` BTC\n"
+                     message = f"🟢🟢 * buy whales just detected `{float(trade['qty']):.2f}` BTC\n"
                      requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage",
                                    data={"chat_id": chat_id, "text": message})
                  else:
-                     message = f"🔴🔴 *whales just detected `{float(trade['qty']):.2f}` BTC\n"
+                     message = f"🔴🔴 *sell whales just detected `{float(trade['qty']):.2f}` BTC\n"
                      requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage",
                                    data={"chat_id": chat_id, "text": message})
-            if float(trade['qty']) > 0.01 and trade['id'] not in processed_trade_ids2:  # Trade volume threshold is set to 1 BTC and check if the trade has been processed before
-                processed_trade_ids2.add(trade['id'])
 
-                if trade['isBuyerMaker']:  # Check if it's a buy or sell order
-                    buy_volume_4h += float(trade['qty'])
-                    buy_volume_1h += float(trade['qty'])
-                    buy_volume_15m += float(trade['qty'])
-                else:
-                    sell_volume_4h += float(trade['qty'])
-                    sell_volume_1h += float(trade['qty'])
-                    sell_volume_15m += float(trade['qty'])
 
-        # Print the total volume of executed large trades for buy and sell
+
         if buy_volume > 30 or sell_volume > 30:
             print(
                 f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} - Large buy orders: {buy_volume:.2f} BTC, Large sell orders: {sell_volume:.2f} BTC")
 
-            net_15m = periods_15m[-1]
-            net_1h = periods_1h[-1]
-            net_4h = periods_4h[-1]
 
-            net_15m_emoji = "🟢" if net_15m > 0 else "🔴"
-            net_1h_emoji = "🟢" if net_1h > 0 else "🔴"
-            net_4h_emoji = "🟢" if net_4h > 0 else "🔴"
 
-            message = f"🟢 *Large buy orders: `{buy_volume:.2f}` BTC*\n🟥 *Large sell orders: `{sell_volume:.2f}` BTC*\n{net_15m_emoji} last 15 min Netvolume: `{net_15m:.2f}` BTC*\n{net_1h_emoji} last 1 h Netvolume: `{net_1h:.2f}` BTC*\n{net_4h_emoji} last 4 h Netvolume: `{net_4h:.2f}` BTC*"
+
+            net_15m_emoji = "🟢" if net_volume_15m > 0 else "🔴"
+            net_1h_emoji = "🟢" if net_volume_1h > 0 else "🔴"
+            net_4h_emoji = "🟢" if net_volume_4h > 0 else "🔴"
+            net_1d_emoji = "🟢" if net_volume_1d > 0 else "🔴"
+            net_3d_emoji = "🟢" if net_volume_3d > 0 else "🔴"
+
+            message = f"🟢 *Large buy orders: `{buy_volume:.2f}` BTC*\n🟥 *Large sell orders: `{sell_volume:.2f}` BTC*\n{net_15m_emoji} last 15 min Netvolume: `{net_volume_15m:.2f}` BTC*\n{net_1h_emoji} last 1 h Netvolume: `{net_volume_1h:.2f}` BTC*\n{net_4h_emoji} last 4 h Netvolume: `{net_volume_4h:.2f}` BTC*\n{net_1d_emoji} last 1 d Netvolume: `{net_volume_1d:.2f}` BTC*\n{net_3d_emoji} last 3d Netvolume: `{net_volume_3d:.2f}` BTC*"
             requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage",
                           data={"chat_id": chat_id, "text": message})
 
             # Reset the variables
         buy_volume = 0
-        # Reset the variables
-        buy_volume = 0
         sell_volume = 0
-        if len(processed_trade_ids2) > 1000:
-            processed_trade_ids2.clear()
-        if len(processed_trade_ids) > 1000:
-            processed_trade_ids.clear()
+        if len(processed_trade_ids2) > 5000:
+            my_list = list(processed_trade_ids2)
+            half_list = my_list[len(my_list) // 2:]
+            processed_trade_ids2 = set(half_list)
+
+        if len(processed_trade_ids) > 5000:
+            my_list = list(processed_trade_ids)
+            half_list = my_list[len(my_list) // 2:]
+            processed_trade_ids = set(half_list)
+
+        current_time = datetime.now()
+        print(net_volume_1h,net_volume_4h)
+        net_15m_emoji = "🟢" if net_volume_15m > 0 else "🔴"
+        net_1h_emoji = "🟢" if net_volume_1h > 0 else "🔴"
+        net_4h_emoji = "🟢" if net_volume_4h > 0 else "🔴"
+        net_1d_emoji = "🟢" if net_volume_1d > 0 else "🔴"
+        net_3d_emoji = "🟢" if net_volume_3d > 0 else "🔴"
+        if (current_time - last_reset_time1h).total_seconds() >= 60 * 60:
+            # Reset the variable to 0 and update the last reset time
+            message = f"{net_15m_emoji} last 15 min Netvolume: `{net_volume_15m:.2f}` BTC*\n{net_1h_emoji} last 1 h Netvolume: `{net_volume_1h:.2f}` BTC*\n{net_4h_emoji} last 4 h Netvolume: `{net_volume_4h:.2f}` BTC*\n{net_1d_emoji} last 1 d Netvolume: `{net_volume_1d:.2f}` BTC*\n{net_3d_emoji} last 3d Netvolume: `{net_volume_3d:.2f}` BTC*"
+            requests.post(f"https://api.telegram.org/bot{bot_token}/sendMessage",
+                          data={"chat_id": chat_id, "text": message})
+        if (current_time - last_reset_time4h).total_seconds() >= 4 * 60 * 60:
+            # Reset the variable to 0 and update the last reset time
+            net_volume_4h = 0
+            last_reset_time4h = current_time
 
 
 
-            # Calculate the net volume for the last 4 hours
 
-        net_volume_4h =+ buy_volume_4h - sell_volume_4h
-        net_volume_1h =+  buy_volume_1h - sell_volume_1h
-        net_volume_15m=+  buy_volume_15m - sell_volume_15m
-        print(net_volume_15m)
-        # Update the periods lists with the net volume for the current periods
-        periods_15m[current_period_index] = net_volume_15m
-        periods_1h[current_period_index] = net_volume_1h
-        periods_4h[current_period_index] = net_volume_4h
+            net_volume_1h = 0
+            last_reset_time1h = current_time
 
-        # Increment the current period index and reset the variables if the current period has ended
-        if time.time() >= (current_period_index + 1) * period_length_15m:
-                current_period_index = (
-                                                   current_period_index + 1) % 10  # Reset the current period index to keep the index within the range of 0 to 9
-                buy_volume_15m = 0
-                sell_volume_15m = 0
-                periods_15m[current_period_index] = 0  # Reset the net volume for the current period to 0
-
-        if time.time() >= (current_period_index + 1) * period_length_1h:
-                current_period_index = (current_period_index + 1) % 10
-                buy_volume_1h = 0
-                sell_volume_1h = 0
-                periods_1h[current_period_index] = 0
-
-        if time.time() >= (current_period_index + 1) * period_length_4h:
-                current_period_index = (current_period_index + 1) % 10
-                buy_volume_4h = 0
-                sell_volume_4h = 0
-                periods_4h[current_period_index] = 0
-
-
-
+        if (current_time - last_reset_time15m).total_seconds() >= 15 * 60:
+            # Reset the variable to 0 and update the last reset time
+            net_volume_15m = 0
+            last_reset_time15m = current_time
+        if (current_time - last_reset_time1d).total_seconds() >= 60*24* 60:
+            # Reset the variable to 0 and update the last reset time
+            net_volume_1d = 0
+            last_reset_time1d = current_time
+        if (current_time - last_reset_time1d).total_seconds() >= 180 * 24 * 60:
+                # Reset the variable to 0 and update the last reset time
+                net_volume_3d = 0
+                last_reset_time1d = current_time
         time.sleep(120)
 
     except Exception as e:
